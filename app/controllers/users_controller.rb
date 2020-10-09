d0c1f02387050955432e8f54cf03e080e60c6433
beckman <beckman.new20@gmail.com>
2020-10-09 13:38:17 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,12 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-
+  
+  def likes
+    @user = User.find(params[:id])
+    @favorites = @user.favorite_microposts.page(params[:page])
+    counts(@user)
+  end
 
   private
 
